b3d1e54c37095f14f0f54f012f7263f317a6a2e5
muratcelikk <muratcelik11@gmail.com>
2025-03-09 03:06:51 +0300

    first integration  commit

---
+++ src/main/java/com/example/springintegration2024/SpringIntegration2024Application.java
@@ -0,0 +1,13 @@
+package com.example.springintegration2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegration2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringIntegration2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springintegration2024/SpringIntegration2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springintegration2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntegration2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
